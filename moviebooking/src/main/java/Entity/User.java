ee6bc2b7fedfe691fd71c61fdeee0c8b55f09f1a
Sai Tejeshwar <123296376+SaiTejeshwar@users.noreply.github.com>
2023-03-08 14:11:59 +0530

    add user entity

---
+++ moviebooking/src/main/java/Entity/User.java
@@ -0,0 +1,35 @@
+package Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="UserTable")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="User_id")
+    @JsonProperty("UserId")
+    private long UserId;
+
+    @Column(name="User_Name")
+    @JsonProperty("UserName")
+    private String UserName;
+    @Column(name="Email")
+    @JsonProperty("Email")
+    private String email;
+
+    @Column(name="password")
+    @JsonProperty("password")
+    private String password;
+}
