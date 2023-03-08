a6a404dcbbbd4a6321864e7bdcdb374a6661a807
Faizan Ahmad <Faizan.Ahmad@Triconinfotech.com>
2023-03-08 15:40:36 +0530

    Theatre Entity updated

--- moviebooking/src/main/java/com/booking/moviebooking/model/Theatre.java
+++ moviebooking/src/main/java/com/booking/moviebooking/model/Theatre.java
@@ -1,6 +1,7 @@
 package com.booking.moviebooking.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,17 +19,29 @@ public class Theatre {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @JsonProperty("theatreId")
     private  Integer theatreId;
 
 
+    @JsonProperty("theatreName")
     private String theatreName;
 
+
+    @JsonProperty("ticketPrice")
     private double ticketPrice;
 
+
+    @JsonProperty("seatCount")
     private  Integer seatCount;
 
 
-    @ManyToMany(cascade = CascadeType.ALL)
+
+    @JsonProperty("movieList")
+    @ManyToMany(cascade = CascadeType.ALL , fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "theatreId"),
+            inverseJoinColumns = @JoinColumn(name = "movieId")
+    )
     private List<Movie> movieList;
 
 }
