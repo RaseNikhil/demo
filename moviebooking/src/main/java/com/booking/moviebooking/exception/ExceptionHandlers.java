b4ce903d6d186bbe633aadddb3ad5143979fd499
Faizan Ahmad <Faizan.Ahmad@Triconinfotech.com>
2023-03-08 22:53:47 +0530

    Custome  password validation is added

--- moviebooking/src/main/java/com/booking/moviebooking/exception/ExceptionHandlers.java
+++ moviebooking/src/main/java/com/booking/moviebooking/exception/ExceptionHandlers.java
@@ -1,15 +1,21 @@
 package com.booking.moviebooking.exception;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ExceptionHandler {
+public class ExceptionHandlers {
 
 
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String , String> userNotFoundExec(UserNotFoundException exception)
     {
        Map<String ,String>  map = new HashMap<>();
