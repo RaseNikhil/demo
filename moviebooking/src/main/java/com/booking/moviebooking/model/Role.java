37984fbd62498a3c24d85d126d6434813901c4ed
Faizan Ahmad <Faizan.Ahmad@Triconinfotech.com>
2023-03-08 15:15:30 +0530

    mapping between role and usetable has been done

--- moviebooking/src/main/java/com/booking/moviebooking/model/Role.java
+++ moviebooking/src/main/java/com/booking/moviebooking/model/Role.java
@@ -1,6 +1,7 @@
 package com.booking.moviebooking.model;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,13 +17,19 @@ public class Role {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("roleId")
     private Integer roleId;
 
+
+    @JsonProperty("role")
 
     private String role;
 
 
-   
+
+    @JsonProperty("userTable")
+    @OneToOne()
+
     private  UserTable userTable;
 
 }
