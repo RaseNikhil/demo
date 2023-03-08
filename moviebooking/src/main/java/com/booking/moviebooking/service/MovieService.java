4c298b3a5c2b60d30b2f08cd44d329a2344c78ff
RaseNikhil <123295150+RaseNikhil@users.noreply.github.com>
2023-03-08 16:21:45 +0530

    MovieService added

--- moviebooking/src/main/java/com/booking/moviebooking/service/MovieService.java
+++ moviebooking/src/main/java/com/booking/moviebooking/service/MovieService.java
@@ -2,10 +2,14 @@ package com.booking.moviebooking.service;
 
 import com.booking.moviebooking.mapper.MovieMapper;
 import com.booking.moviebooking.model.Movie;
-import com.booking.moviebooking.model.MovieRequest;
+import com.booking.moviebooking.utility.MovieRequest;
 import com.booking.moviebooking.repository.MovieRepository;
+import com.booking.moviebooking.utility.MovieResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,12 +17,26 @@ public class MovieService {
     MovieRepository movieRepository;
 
     public String addMovie(MovieRequest movieRequest){
-        Movie movieData = movieRepository.findByMovieid(movieRequest.getUserId());
+        Optional<Movie> movieData = movieRepository.findById(movieRequest.getUserId());
         if(movieData==null){
             movieRepository.save(MovieMapper.movieMapper.toEntity(movieRequest));
             return "Movie Added";
         }else {
             return "movie Already Present";
         }
+    }
+
+    public MovieResponse getMovieById(int id){
+        Optional<Movie> movieData=movieRepository.findById(id);
+        if(movieData.isPresent()){
+            return MovieMapper.movieMapper.toResponse(movieData.get());
+        }else{
+            return null;
+        }
+    }
+
+    public List<MovieResponse> getAllMovies(){
+        List<MovieResponse> allMovies = MovieMapper.movieMapper.toResponseList(movieRepository.findAll());
+        return allMovies;
     }
 }
